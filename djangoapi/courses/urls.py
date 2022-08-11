e4f803168cc6fe603d4ec4f3494197334fab3bfc
RifadulHaque <rifadulhaque1@gmail.com>
2022-08-10 23:28:58 -0400

    Added Hyperlinked Serializers

--- djangoapi/courses/urls.py
+++ djangoapi/courses/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseView) # the link is localhost:800/courses because the name is added here
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
